b617f032cf3f2f6858d679d61a36dadcd7aa2319
Jakub Bogusz <qboosh@pld-linux.org>
2019-05-26 12:24:13 +0200

    - update vendor.js files; release 3

--- vendor-ac.js
+++ vendor-ac.js
@@ -5,6 +5,6 @@ pref("general.useragent.vendorComment", "Ac");
 pref("general.useragent.compatMode.firefox", true);
 pref("distribution.searchplugins.defaultLocale", "en-US");
 // Forbid application updates
-lockPref("app.update.enabled", false);
+pref("app.update.enabled", false, locked);
 // POODLE protection, CVE-2014-3566
 pref("security.tls.version.min", 1);
